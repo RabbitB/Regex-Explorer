8a5f6b8f9580871cddd5dafe56fc27ee4c1b3eb1
Rabbit <rabbit@dfaction.net>
2019-05-09 13:57:01 -0700

    Cleanup Explorer script and add double-click navigation.
    Performed some minor cleanup on the main file explorer script, as well as added double-click navigation to the original file view.

--- Explorer.gd
+++ Explorer.gd
@@ -1,25 +1,27 @@
 extends Control
 
+const ExplorerAddressBar: Script = preload("res://explorer_address_bar/explorer_address_bar.gd")
 const RegexRuleContainer: Script = preload("res://regex_rule_container/regex_rule_container.gd")
 
 export (Color) var InvalidFileNameHighlightColor = Color8(211, 71, 74)
 export (Color) var ModifiedFileNameHighlightColor = Color8(236, 131, 60)
 
-onready var ValidDirIndicator: ColorRect = $MainVBoxContainer/ValidDirIndicator as ColorRect
-onready var OriginalFileList: ItemList = $MainVBoxContainer/FileSplitContainer/RuleSplitContainer/OriginalFileList as ItemList
-onready var PreviewFileList: ItemList = $MainVBoxContainer/FileSplitContainer/PreviewFileList as ItemList
-onready var RegexRuleController: RegexRuleContainer = $MainVBoxContainer/FileSplitContainer/RuleSplitContainer/RegexRuleScrollContainer/RegexRuleContainer as RegexRuleContainer
+onready var _AddressBar: ExplorerAddressBar = $MainVBoxContainer/ExplorerAddressBar as ExplorerAddressBar
+onready var _ValidDirIndicator: ColorRect = $MainVBoxContainer/ValidDirIndicator as ColorRect
+onready var _OriginalFileList: ItemList = $MainVBoxContainer/FileSplitContainer/RuleSplitContainer/OriginalFileList as ItemList
+onready var _PreviewFileList: ItemList = $MainVBoxContainer/FileSplitContainer/PreviewFileList as ItemList
+onready var _RegexRuleController: RegexRuleContainer = $MainVBoxContainer/FileSplitContainer/RuleSplitContainer/RegexRuleScrollContainer/RegexRuleContainer as RegexRuleContainer
 
 var _target_dir: Directory = Directory.new()
 
 
 func _update_file_output():
 
-	PreviewFileList.clear()
+	_PreviewFileList.clear()
 
-	for i in range(OriginalFileList.get_item_count()):
+	for i in range(_OriginalFileList.get_item_count()):
 
-		var new_file_name: String = RegexRuleController.process_string(OriginalFileList.get_item_text(i), OriginalFileList.is_selected(i), true)
+		var new_file_name: String = _RegexRuleController.process_string(_OriginalFileList.get_item_text(i), _OriginalFileList.is_selected(i), true)
 		var sanitized_file_name = _sanitize_file_name(new_file_name)
 
 		var validating_regex: RegEx = RegEx.new()
@@ -29,30 +31,30 @@ func _update_file_output():
 
 		if is_valid_name && !is_duplicate_name:
 
-			PreviewFileList.add_item(sanitized_file_name)
+			_PreviewFileList.add_item(sanitized_file_name)
 
 			if sanitized_file_name != new_file_name:
 
-				PreviewFileList.set_item_custom_fg_color(i, ModifiedFileNameHighlightColor)
-				PreviewFileList.set_item_tooltip(i, "Filename \"%s\" is invalid. Auto-corrected to valid name." % new_file_name)
+				_PreviewFileList.set_item_custom_fg_color(i, ModifiedFileNameHighlightColor)
+				_PreviewFileList.set_item_tooltip(i, "Filename \"%s\" is invalid. Auto-corrected to valid name." % new_file_name)
 
 		else:
 
-			PreviewFileList.add_item(OriginalFileList.get_item_text(i))
-			PreviewFileList.set_item_disabled(i, true)
-			PreviewFileList.set_item_custom_fg_color(i, InvalidFileNameHighlightColor)
+			_PreviewFileList.add_item(_OriginalFileList.get_item_text(i))
+			_PreviewFileList.set_item_disabled(i, true)
+			_PreviewFileList.set_item_custom_fg_color(i, InvalidFileNameHighlightColor)
 
 			if is_duplicate_name:
 
-				PreviewFileList.set_item_text(i, sanitized_file_name)
-				PreviewFileList.set_item_tooltip(i, "Filename already exists. Cannot rename.")
+				_PreviewFileList.set_item_text(i, sanitized_file_name)
+				_PreviewFileList.set_item_tooltip(i, "Filename already exists. Cannot rename.")
 
 			else:
 
-				PreviewFileList.set_item_tooltip(i, "Filename invalid. Could not auto-correct into valid name.")
+				_PreviewFileList.set_item_tooltip(i, "Filename invalid. Could not auto-correct into valid name.")
 
-		var file_icon: Texture = OriginalFileList.get_item_icon(i)
-		if file_icon: PreviewFileList.set_item_icon(i, file_icon)
+		var file_icon: Texture = _OriginalFileList.get_item_icon(i)
+		if file_icon: _PreviewFileList.set_item_icon(i, file_icon)
 
 
 func _sanitize_file_name(file_name: String) -> String:
@@ -76,7 +78,7 @@ func _sanitize_file_name(file_name: String) -> String:
 
 func read_dir_contents(dir_path: String) -> void:
 
-	OriginalFileList.clear()
+	_OriginalFileList.clear()
 	assert(_target_dir.dir_exists(dir_path))
 
 	_target_dir.open(dir_path)
@@ -88,11 +90,11 @@ func read_dir_contents(dir_path: String) -> void:
 
 		if _target_dir.current_is_dir():
 
-			OriginalFileList.add_item(next_item, preload("res://directory_icon.png"))
+			_OriginalFileList.add_item(next_item, preload("res://directory_icon.png"))
 
 		else:
 
-			OriginalFileList.add_item(next_item)
+			_OriginalFileList.add_item(next_item)
 
 		next_item = _target_dir.get_next()
 
@@ -101,32 +103,40 @@ func read_dir_contents(dir_path: String) -> void:
 
 func preview_list_contains_item_text(item_text: String) -> int:
 
-	for i in range(PreviewFileList.get_item_count()):
-		if PreviewFileList.get_item_text(i) == item_text: return i
+	for i in range(_PreviewFileList.get_item_count()):
+		if _PreviewFileList.get_item_text(i) == item_text: return i
 
 	return -1
 
 
 func _on_ExplorerAddressBar_path_changed(path: String, is_path_valid: bool):
 
-	ValidDirIndicator.valid = is_path_valid
+	_ValidDirIndicator.valid = is_path_valid
 	if is_path_valid: read_dir_contents(path)
 
 
 func _on_RenameFilesButton_pressed():
 
-	for i in range(PreviewFileList.get_item_count()):
+	for i in range(_PreviewFileList.get_item_count()):
 
-		if PreviewFileList.is_item_disabled(i): continue
+		if _PreviewFileList.is_item_disabled(i): continue
 
-		var new_file_path: String = _target_dir.get_current_dir().plus_file(PreviewFileList.get_item_text(i))
-		var old_file_path: String = _target_dir.get_current_dir().plus_file(OriginalFileList.get_item_text(i))
+		var new_file_path: String = _target_dir.get_current_dir().plus_file(_PreviewFileList.get_item_text(i))
+		var old_file_path: String = _target_dir.get_current_dir().plus_file(_OriginalFileList.get_item_text(i))
 
 		_target_dir.rename(old_file_path, new_file_path)
 		read_dir_contents(_target_dir.get_current_dir())
+
 
 func _on_OriginalFileList_multi_selected(index, selected):
 
 	_update_file_output()
 
+
+func _on_OriginalFileList_item_activated(index):
+
+	var new_dir_path: String = _target_dir.get_current_dir().plus_file(_OriginalFileList.get_item_text(index))
+
+	if _target_dir.dir_exists(new_dir_path):
+		_AddressBar.update_path(new_dir_path, true)
 
