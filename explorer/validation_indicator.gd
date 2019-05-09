8c41896482df957e53676c2a39b4a4bad68c3e0e
Rabbit <rabbit@dfaction.net>
2019-05-09 14:12:13 -0700

    Cleanup Validation Indicator script.
    A quick cleanup of the Validation Indicator script, to meet Godot naming conventions and format standards.

--- explorer/validation_indicator.gd
+++ explorer/validation_indicator.gd
@@ -1,26 +1,31 @@
 extends CanvasItem
 
-export (bool) var valid: bool = false setget _on_valid_change
-export (bool) var standby: bool = false setget _on_standby_change
+export (bool) var valid: bool = false setget _set_valid
+export (bool) var standby: bool = false setget _set_standby
 export (Color) var valid_color: Color = Color8(71, 211, 103)
 export (Color) var invalid_color: Color = Color8(211, 71, 74)
 export (Color) var standby_color: Color = Color8(236, 131, 60)
 
-func _ready():
-	
-	update_color()
 
-func _on_valid_change(is_valid: bool) -> void:
+func _set_valid(is_valid: bool) -> void:
 
 	valid = is_valid
 	update_color()
-	
-func _on_standby_change(is_standby: bool) -> void:
-	
+
+
+func _set_standby(is_standby: bool) -> void:
+
 	standby = is_standby
 	update_color()
 
+
+func _ready():
+
+	update_color()
+
+
 func update_color():
-	
+
 	self.self_modulate = valid_color if valid else invalid_color
 	self.self_modulate = standby_color if standby else self.self_modulate
+
