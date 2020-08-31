2aa19aea083e96eb020314cb302d131db63c1587
Rabbit <rabbit@dfaction.net>
2020-08-30 20:39:51 -0700

    Minor code styling of ValidationIndicator

--- Project/explorer/validation_indicator.gd
+++ Project/explorer/validation_indicator.gd
@@ -1,5 +1,6 @@
 extends CanvasItem
 
+
 export (bool) var valid: bool = false setget _set_valid
 export (bool) var standby: bool = false setget _set_standby
 export (Color) var valid_color: Color = Color8(71, 211, 103)
@@ -7,25 +8,21 @@ export (Color) var invalid_color: Color = Color8(211, 71, 74)
 export (Color) var standby_color: Color = Color8(236, 131, 60)
 
 
-func _set_valid(is_valid: bool) -> void:
+func _ready() -> void:
+	update_color()
 
+
+func update_color():
+	self.self_modulate = valid_color if valid else invalid_color
+	self.self_modulate = standby_color if standby else self.self_modulate
+
+
+func _set_valid(is_valid: bool) -> void:
 	valid = is_valid
 	update_color()
 
 
 func _set_standby(is_standby: bool) -> void:
-
 	standby = is_standby
 	update_color()
-
-
-func _ready():
-
-	update_color()
-
-
-func update_color():
-
-	self.self_modulate = valid_color if valid else invalid_color
-	self.self_modulate = standby_color if standby else self.self_modulate
 
